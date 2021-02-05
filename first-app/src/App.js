fc51b2df8fce5aaa99b21538aa8328821bfbe77e
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-02-05 22:33:35 +0200

    create +10 & -10 counters

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -17,17 +17,21 @@ function App() {
 }
 
 function Plus(props) {
+  const handleClick = () => props.onClickFunctionPlus(props.increment);
+
   return (
-    <button onClick={props.onClickFunctionPlus}>
-      +1
+    <button onClick={handleClick}>
+      +{props.increment}
     </button>
   );
 }
 
 function Minus(props) {
+  const handleClick = () => props.onClickFunctionMinus(props.decrement);
+
   return (
-    <button onClick={props.onClickFunctionMinus}>
-      -1
+    <button onClick={handleClick}>
+      -{props.decrement}
     </button>
   );
 }
@@ -42,22 +46,24 @@ function Reset(props) {
 
 function Display(props) {
   return (
-    <div>{props.message}</div>
+    <div>{props.result}</div>
   );
 }
 
 function Application() {
   const [counter, setCounter] = useState(2021);
-  const incrementCounter = () => setCounter(counter+1);
-  const decrementCounter = () => setCounter(counter-1);
+  const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
+  const decrementCounter = (decrementValue) => setCounter(counter-decrementValue);
   const origin = () => setCounter(2021);
   
   return (
     <div>
-      <Plus onClickFunctionPlus={incrementCounter}/>
-      <Minus onClickFunctionMinus={decrementCounter}/>
+      <Plus onClickFunctionPlus={incrementCounter} increment={1}/>
+      <Plus onClickFunctionPlus={incrementCounter} increment={10}/>
+      <Minus onClickFunctionMinus={decrementCounter} decrement={1}/>
+      <Minus onClickFunctionMinus={decrementCounter} decrement={10}/>
       <Reset resetValue={origin}/>
-      <Display message={counter}/>
+      <Display result={counter}/>
     </div>
   );
 }
