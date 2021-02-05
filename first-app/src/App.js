9d7ffd0d1871f6b807b6effecc90990b09c4d222
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-02-05 23:41:02 +0200

    edit app name index.html & edit app component App.js

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -3,19 +3,6 @@ import './App.css';
 import reactDom from 'react-dom';
 import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <h1 id="ele">Counter</h1>
-
-      </header>
-    </div>
-  );
-}
-
 function Plus(props) {
   const handleClick = () => props.onClickFunctionPlus(props.increment);
 
@@ -50,27 +37,38 @@ function Display(props) {
   );
 }
 
-function Application() {
+function App() {
   const [counter, setCounter] = useState(2021);
   const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
   const decrementCounter = (decrementValue) => setCounter(counter-decrementValue);
   const origin = () => setCounter(2021);
   
   return (
-    <div>
-      <Plus onClickFunctionPlus={incrementCounter} increment={1}/>
-      <Plus onClickFunctionPlus={incrementCounter} increment={10}/>
-      <Minus onClickFunctionMinus={decrementCounter} decrement={1}/>
-      <Minus onClickFunctionMinus={decrementCounter} decrement={10}/>
-      <Reset resetValue={origin}/>
-      <Display result={counter}/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>Counter</h1>
+        <div>
+          <Plus onClickFunctionPlus={incrementCounter} increment={1}/>
+          <Plus onClickFunctionPlus={incrementCounter} increment={10}/>
+          <Minus onClickFunctionMinus={decrementCounter} decrement={1}/>
+          <Minus onClickFunctionMinus={decrementCounter} decrement={10}/>
+          <Reset resetValue={origin}/>
+          <hr></hr>
+          <div>
+            <Display result={counter}/>
+          </div>
+        </div>
+
+      </header>
     </div>
   );
 }
 
 reactDom.render(
-  <Application />,
-  document.getElementById('ele')
+  <App />,
+  document.getElementById("root")
 );
 
 export default App;
