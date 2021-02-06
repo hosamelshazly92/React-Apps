aa114417ead64d9b6731f35005b44f84fe6ad59c
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-02-06 03:47:23 +0200

    edit app name index.html & add CreateNewElement function App.js

--- display-timestamp/src/App.js
+++ display-timestamp/src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { createElement } from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <div>
+          <CreateNewElement />
+        </div>
       </header>
     </div>
   );
 }
+
+function CreateNewElement() {
+  return(
+    React.createElement(
+      "div",
+      null,
+      "Display Timestamp",
+      React.createElement('h2', null, "Time"),
+      React.createElement('pre', null, (new Date).toLocaleTimeString())
+    )
+  );
+}
+
+const render = () => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
+}
+
+setInterval(render, 1000);
 
 export default App;
