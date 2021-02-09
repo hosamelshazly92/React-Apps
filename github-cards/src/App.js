fb70c03dffae36990564a9c56ee42e8d1c0976b9
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-02-09 08:01:11 +0200

    create Card class component App.js

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -3,7 +3,22 @@ import './App.css';
 import reactDom from 'react-dom';
 import React from 'react';
 
-// class component
+// card component
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="github-profile" style={{ margin: '1rem' }}>
+        <img src="https://placehold.it/75"></img>
+        <div className="info" style={{ display: 'inline-block', marginLeft: '10px' }}>
+          <div className="name" style={{ fontSize: "125%" }}>Name</div>
+          <div className="company" style={{marginLeft: '10px'}}>Company</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// App class component
 class App extends React.Component {
   // constructor
   // this
@@ -11,12 +26,13 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1 className="header">{this.props.title}</h1>
+        <Card />
       </div>
     );
   }
 }
 
-// function component
+// App function component
 // const App = ({title}) => {
 //   return (
 //     <div className="App">
