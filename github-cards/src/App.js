a86e1807be990df6d04b21a9ca0d886eb913532e
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-02-09 08:35:15 +0200

    create Form class component App.js

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -3,28 +3,59 @@ import './App.css';
 import reactDom from 'react-dom';
 import React from 'react';
 
+// test data
+const testData = [
+  {
+    name: "Hosam Elshazly",
+    bio: "UX/UI Designer",
+    location: "Egypt",
+    avatar_url: "https://avatars.githubusercontent.com/u/59311235?v=4"
+  },
+  {
+    name: "Dan Abramov",
+    bio: "Developer",
+    location: null,
+    avatar_url: "https://avatars.githubusercontent.com/u/810438?v=4"
+  }
+];
+
 // card class component
 class Card extends React.Component {
   render() {
+    const profile = this.props;
+
     return (
       <div className="github-profile" style={{ margin: '1rem' }}>
-        <img src="https://placehold.it/75"></img>
-        <div className="info" style={{ display: 'inline-block', marginLeft: '10px' }}>
-          <div className="name" style={{ fontSize: "125%" }}>Name</div>
-          <div className="company" style={{marginLeft: '10px'}}>Company</div>
+        <img src={profile.avatar_url} style={{ height: '75px' }}></img>
+        <div className="info" style={{ display: 'inline-block', textAlign: 'left', marginLeft: '10px' }}>
+          <div style={{ fontSize: '18px', fontWeight: 'bold' }}>{profile.name}</div>
+          <div style={{ fontSize: '12px' }}>{profile.bio}</div>
+          <div style={{ fontSize: '12px' }}>{profile.location}</div>
         </div>
       </div>
     );
   }
 }
 
-// car list class component
+// card list class component
 const CardList = (props) => {
   return (
     <div>
-      <Card />
+      {testData.map(profile => <Card {...profile}/>)}
     </div>
   );
+}
+
+// form class component
+class Form extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Enter GitHub Usermane...." />
+        <button>Add New Card</button>
+      </form>
+    );
+  }
 }
 
 // App class component
@@ -35,6 +66,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1 className="header">{this.props.title}</h1>
+        <Form />
         <CardList />
       </div>
     );
