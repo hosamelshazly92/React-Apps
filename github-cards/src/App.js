24aae8e10245c832a5757c8fceeaad24e4878ed1
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-02-09 07:33:55 +0200

    create App class component App.js

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import reactDom from 'react-dom';
+import React from 'react';
 
-const App = ({title}) => {
-  return (
-    <div className="App">
-      <h1 className="header">{title}</h1>
-    </div>
-  );
+// class component
+class App extends React.Component {
+  // constructor
+  // this
+  render() {
+    return (
+      <div className="App">
+        <h1 className="header">{this.props.title}</h1>
+      </div>
+    );
+  }
 }
+
+// function component
+// const App = ({title}) => {
+//   return (
+//     <div className="App">
+//       <h1 className="header">{title}</h1>
+//     </div>
+//   );
+// }
 
 reactDom.render(
   <App title="GitHub Cards App"/>,
