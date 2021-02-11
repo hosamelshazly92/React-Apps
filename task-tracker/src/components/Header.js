00150209faacc6d116e3056c065a807223f7c3e8
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-02-11 09:48:21 +0200

    create Button component App.js

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,7 +1,14 @@
+import Button from './Button.js';
+
 const Header = ({title}) => {
+    const onClick = (e) => {
+        console.log(`X Psition: ${e.pageX}, Y Position: ${e.pageY}`);
+    }
+
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick}/>
         </header>
     )
 }
