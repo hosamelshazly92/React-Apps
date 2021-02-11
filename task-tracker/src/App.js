b349c62db8734680ea86dfa34a8dbe94bf12af4e
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-02-11 12:37:30 +0200

    install json-server & create db.json

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,33 +1,28 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 function App() {
-  const data = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting',
-        day: 'Jan 12th',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Shoppnig',
-        day: 'Jan 12th',
-        reminder: false,
-    }
-  ];
-
-  const [tasks, setTasks] = useState(data);
-
+  const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+
+    getTasks();
+  }, []);
+
+  // fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+
+    return data
+  }
 
   // add task
   const addTask = (task) => {
@@ -53,7 +48,7 @@ function App() {
         onCreate={() => 
         setShowAddTask(!showAddTask)} 
         showAdd={showAddTask}
-        />
+      />
       {showAddTask ? <AddTask onCreate={addTask} /> : ''}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "No tasks to display"}
     </div>
